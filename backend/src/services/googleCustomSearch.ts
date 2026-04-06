bc0e0bee7ede2076d3f5d8ad5e3c82599eddd522
Sameer Patel <samxpatel2@gmail.com>
2026-04-06 23:03:56 +0530

    add try catch in custom search

--- backend/src/services/googleCustomSearch.ts
+++ backend/src/services/googleCustomSearch.ts
@@ -1,9 +1,14 @@
-import  { tavily  } from "@tavily/core" ;
+import { tavily } from "@tavily/core";
 import { config } from "../config/config.js";
-
 console.log('Tavily API Key:', config.tavily.apiKey);
 const tvly = tavily({ apiKey: config.tavily.apiKey as string });
 
-const response = await tvly.search("https://clutch.co/");
-
-console.log(response);
+export async function tavilySearch(query :string){
+  try{
+    const response = await tvly.search(query);
+    console.log(response);
+  }
+  catch(e){
+    console.log("Error in tavily api : " , e)
+  }
+}
